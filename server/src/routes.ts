627ad1e9630bace4f1b48ad90e3f10185f62e49f
casfalking <thecaasantos@gmail.com>
2023-01-30 01:00:14 -0300

    feat: get habits route

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -4,7 +4,11 @@ import { z } from 'zod';
 import { prisma } from './lib/prisma'
 
 export async function appRoutes(app: FastifyInstance) {
+    
+    
+    //Create Habit
     app.post('/habits', async (request) => {
+        
         const createHabitBody = z.object({
             title: z.string(),
             weekDays: z.array(
@@ -28,6 +32,44 @@ export async function appRoutes(app: FastifyInstance) {
                     }),
                 },
             }
+        }) 
+    })
+
+
+    //Get day
+
+    app.get('/day1', async (request) => {
+        
+        const getDayParams = z.object({
+            date: z.coerce.date(),
         })
-    }
+
+        const { date } getDayParams.parse(request.query)
+
+        const parsedDate = dayjs(date).startOf('day')
+        const weekDay = parsedDate.get('day')
+
+        const possibleHabitis = await prisma.habit.findMany({
+            where: {
+                created_at: {
+                    lte: date,
+                },
+                weekDays: {
+                    some: {
+                        week_day: weekDay,
+                    }
+                }
+            },
+        })
+
+        const day = await prisma.habit.findFirst({
+            where: {
+                date: parsedDate.toDate(),
+            },
+            include: {
+                dayHabits: true,
+            }
+        })
+    })
+
 }
